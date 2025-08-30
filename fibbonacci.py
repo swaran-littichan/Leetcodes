5de59162a38f2656fa7ba553580a14f5b32882a7
Swaran Littichan <130770861+swaran-littichan@users.noreply.github.com>
2025-08-30 19:32:59 +0530

    Add files via upload

---
+++ fibbonacci.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def fib(self, n):
+       a,b=0,1
+       if n==0:
+        return a
+       elif n==1:
+        return b
+       for i in range(2,n+1):
+           a,b=b,a+b
+       return b
